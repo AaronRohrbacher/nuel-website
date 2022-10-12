005379b36c95c08394dc0d578d11dd1da08129c8
Aaron Rohrbacher <arjazz@icloud.com>
2022-10-11 17:57:13 -0700

    adds build scripting via gulp and bash

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
 var open = require('gulp-open');
 var fileinclude = require('gulp-file-include');
+var cleanCSS = require('gulp-clean-css');
+var htmlmin = require('gulp-htmlmin');
 
 var Paths = {
   HERE: './',
@@ -12,41 +14,40 @@ var Paths = {
   CSS: './assets/css/',
   SCSS_TOOLKIT_SOURCES: './assets/scss/argon-design-system.scss',
   SCSS: './assets/scss/**/**',
-  scripts: {
-    src: './',
-    dest: './build/'
-  }
 };
 
 gulp.task('compile-html', function() {
   return gulp.src([
-    '*.html',
+    './**/*.html',
+    './**/config-title-meta.json',
     '!header.html', // ignore
-    '!footer.html' // ignore
+    '!footer.html', // ignore
+    '!./node_modules/**'
     ])
     .pipe(fileinclude({
       prefix: '@@',
       basepath: '@file'
     }))
-    .pipe(gulp.dest(Paths.scripts.dest));
+    .pipe(gulp.dest('./pre-build'));
 });
 
 gulp.task('compile-scss', function() {
   return gulp.src(Paths.SCSS_TOOLKIT_SOURCES)
     .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(autoprefixer())
     .pipe(sourcemaps.write(Paths.HERE))
-    .pipe(gulp.dest(Paths.CSS));
+    .pipe(gulp.dest('./pre-build/assets'));
 });
 
-gulp.task('watch', function() {
-  gulp.watch(Paths.SCSS, gulp.series('compile-scss'));
+gulp.task('build-html', function () {
+  return gulp.src('./pre-build/**/*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('./build'))
 });
 
-gulp.task('open', function() {
-  gulp.src('index.html')
-    .pipe(open());
+gulp.task('build-css', function() {
+  return gulp.src('./assets/**/*.css')
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('./build/assets'))
 });
-
-gulp.task('open-app', gulp.parallel('open', 'watch'));
---
+++ script.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+shopt -s globstar
+rm -rf pre-build
+rm -rf build
+gulp compile-scss
+gulp compile-html
+cd pre-build
+absolute_path="$PWD"
+for dir in **/; do
+    title=$(jq .page.title config-title-meta.json)
+    replace {{title}} "$title" *
+    echo "$dir"
+    cd "$absolute_path/$dir"
+    if test -f ./config-title-meta.json; then
+        title=$(jq .page.title config-title-meta.json)
+    else
+        title=$(jq .page.title ../config-title-meta.json)
+    fi
+    replace {{title}} "$title" *
+    cd $absolute_path
+done
+gulp build-css
+gulp build-html
